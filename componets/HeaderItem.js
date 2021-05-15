3e1aceb163337155a2a55a7b4e1f6ccab8646617
Adnan <adnahl@gmail.com>
2021-05-15 16:02:10 -0500

    Ajustando estilo Menu superior

---
+++ componets/HeaderItem.js
@@ -0,0 +1,12 @@
+function HeaderItem({ Title, Icon }) {
+  return (
+    <div className="group flex flex-col items-center cursor-pointer w-12 max-w-max hover:text-white ">
+      <Icon className="h-8 mt-4 mb-1 group-hover:animate-bounce" />
+      <p className="opacity-0 font-semibold group-hover:opacity-100 tracking-widest">
+        {Title}
+      </p>
+    </div>
+  )
+}
+
+export default HeaderItem
--- pages/index.js
+++ pages/index.js
@@ -14,26 +14,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/*putillo css*/}
-
-
 
       <Header />
 
 
-      {/*
 
-      <div class="mt-1 pt-4 relative max-w-full sm:max-w-max mr-8">
-        <input
-          class="w-full border-2 border-white bg-[#130282] h-10 ml-4 px-4 pr-32 rounded text-sm focus:outline-none"
-          type="search" name="search" placeholder="Search" />
-
-        <button type="submit" class="absolute right-0 top-0 mt-5 mr-3">
-          <SearchIcon className="h-8" />
-        </button>
-      </div>
-
-*/}
 
 
 
