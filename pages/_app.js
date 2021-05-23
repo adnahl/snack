b92a649aeb4c1ded09c81f5797c22fff99c0e864
Adnan <adnahl@gmail.com>
2021-05-23 18:13:55 -0500

    Quitado el menu

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
 import '../styles/globals.css'
-import Header from '../componets/Header'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="bg-blue-0 -mt-4">
-      <Header />
+
       <Component {...pageProps} />
     </div>
   )
